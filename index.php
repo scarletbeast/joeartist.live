d2567374c658b289c63813c1b7d0e3c87e5a30fe
Bryan Ruiz <bryan@scarletbeast.com>
2023-09-20 18:07:21 +0400

    only commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Joe Artist's Music Art</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<div class="video-container">
+    <video playsinline autoplay muted loop id="bgvid">
+        <source src="assets/dj-party-video.mp4" type="video/mp4">
+    </video>
+    <div class="head">
+        <a href="index.html"><img class="logo" src="assets/joe-artist-logo.gif"></a>
+        <ul>
+            <li><a class="selected" href="index.php">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#shows">Shows</a></li>
+            <li><a href="#discogrpahy">Discography</a></li>
+            <li><a href="#gallery">Gallery</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </div>
+    <div class="overlay">
+            <h2>Joe Artist<br><span>Official Website</span></h2>
+    </div>
+</div>
+</body>
+</html>
